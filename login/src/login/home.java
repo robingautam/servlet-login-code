59fd5f4e7dea938c7641411a3851e1c38ad66c88
robingautam <31219114+robingautam@users.noreply.github.com>
2018-05-11 19:54:44 +0530

    Update home.java

--- login/src/login/home.java
+++ login/src/login/home.java
@@ -2,8 +2,12 @@ package login;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-import javax.servlet.RequestDispatcher;
+//import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +23,7 @@ public class home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-          
+		  response.setContentType("text/html"); 
 		HttpSession session = request.getSession();
 		String name = session.getAttribute("name").toString();
 		String email = session.getAttribute("email").toString();
@@ -41,7 +45,9 @@ public class home extends HttpServlet {
 				+ ""
 				+ "textarea{"
 				+ ""
-				+ "width: 500px; height: 100px;}</style></head>");
+				+ "width: 500px; height: 100px;}#status{ background-color: white; width: 500px; height: auto; margin-left: 400px; margin-top: 5px;}body{"
+				+ ""
+				+ "background-color: #ddd;}</style></head>");
 		out.println("<body><div id='wrapper'></div><br><br>");
 		out.println("<h1>Welcome "+name+"<br></h1><br><br>");
 		out.println("Email  "+email+"<br><br>"+"Name "+name+"<br><br>");
@@ -49,11 +55,34 @@ public class home extends HttpServlet {
 				+ ""
 				+ "<form method='post' action='profile'>"
 				+ ""
-				+ "<textarea name='text'></textarea><br>"
+				+ "<textarea name='textarea'></textarea><br>"
 				+ ""
 				+ "<input type='submit' name='post' value='Post'></form> </div>");
 		out.println("<a href='logout'>Logout</a>");
 		out.println("</body></html>");
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\chatbook.db");
+			Statement statement = conn.createStatement();
+	          ResultSet result = statement.executeQuery("SELECT user.name, post.* FROM user, post WHERE post.email = '"+email+"' AND user.email = '"+email+"' ORDER BY pid DESC");
+	          while(result.next()) {
+	        	  String name1 = result.getString("name");
+	        	  String posttext = result.getString("text");
+	        	  int id = result.getInt("pid");
+	        	  out.println("<div id ='status'>"
+	        	  		+ ""
+	        	  		+ "<p1 style='text-style: bold; margin-left: 10px;'><strong>"+name1+"</strong></p1><a "
+	        	  				+ ""
+	        	  				+ "style='float: right; margin-right: 15px; text-decoration: none; font-size: 13px;'href='delete?pid="+id+"'>Delete</a><br><br><p2 style='margin-left: 10px !important;"
+	        	  						+ ""
+	        	  						+ "margin-right: 10px !important;'>"+posttext+"</p2></div>");
+	        	  
+	          }
+	          
+		}
+		catch(Exception e) {
+		out.println("value not fetched" +e.getMessage());
+			
+		}
 		
 		}
 		
